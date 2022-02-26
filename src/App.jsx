81b5668479d63ae5f0337cb4f0b6f42172f120a2
Angel <angelriera1796@gmail.com>
2022-02-26 15:30:03 -0400

    navbar and header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Header/>
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div class="
+    min-w-full 
+    bg-[url('https://static.wixstatic.com/media/ea71bb_f6afb11f2c8f406ca1c8993c2eca99e7~mv2_d_11495_6131_s_4_2.jpg/v1/fill/w_980,h_671,al_t,q_85,usm_0.66_1.00_0.01/ea71bb_f6afb11f2c8f406ca1c8993c2eca99e7~mv2_d_11495_6131_s_4_2.webp')]
+    bg-no-repeat bg-cover min-w-full min-h-screen 
+    flex justify-center items-center flex-col
+    "
+        >
+
+                <h1 className='text-red-600 text-6xl font-semibold'>MarketPlace</h1>
+
+                <button class="px-6 mt-8 p-3 border-2 border-white hover:bg-gray-100">
+                Shop the collection
+                </button>
+        </div>
+    )
+}
+
+export default Header
